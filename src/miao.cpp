ef4d85e17166bec258391800d561279fac4cb4a6
Gaosx <152193188+Gao-Leonhard-Euler@users.noreply.github.com>
2025-11-14 17:28:21 +0800

    修复了浮点数处理问题
    修复了浮点数处理问题

--- src/miao.cpp
+++ src/miao.cpp
@@ -14,8 +14,8 @@ void readlines(char* filename,vector<vector<pair<bool,string>>>&lines){
     s.clear();e.clear();
     lines.push_back(vector<pair<bool,string>>());
     size_t cnt=0;
-    int type=1;
-    // 1:空串，2:字母/数字/下划线，3:运算符!%^|&*-+=<>./，4:引号内字符串，5:单行注释，6:多行注释，7:宏
+    int type=0;
+    // 0:空串，1:纯数字，2:字母/数字/下划线，3:运算符!%^|&*-+=<>./，4:引号内字符串，5:单行注释，6:多行注释，7:宏
     // 负数：存在反斜杠
     // 立即符号：([{?:~,}]);
     while(!feof(fp)){
@@ -29,10 +29,10 @@ void readlines(char* filename,vector<vector<pair<bool,string>>>&lines){
             if(type==4||type==5||type==6||type==7)s+=c;
             else{
                 e+=c;
-                if(type!=1){
+                if(type){
                     lines[cnt].push_back(make_pair(1,s));
                     s.clear();
-                    type=1;
+                    type=0;
                 }
             }
         }
@@ -44,11 +44,11 @@ void readlines(char* filename,vector<vector<pair<bool,string>>>&lines){
             else if(type<0)type=-type;
             else if(type==6)s+='\n';
             else{
-                if(type!=1)lines[cnt].push_back(make_pair(type==2||type==3||type==4,s));
+                if(type)lines[cnt].push_back(make_pair(type==1||type==2||type==3||type==4,s));
                 cnt++;
                 lines.push_back(vector<pair<bool,string>>());
                 s.clear();e.clear();
-                type=1;
+                type=0;
             }
         }
         else if(c=='\\'){
@@ -63,12 +63,12 @@ void readlines(char* filename,vector<vector<pair<bool,string>>>&lines){
                 type=-type;
                 if(type==4||type==5||type==6||type==7)s+='\\';
             }
-            if(type==2||type==3){
+            if(type==1||type==2||type==3){
                 lines[cnt].push_back(make_pair(1,s));
                 s.clear();
-                type=1;
+                type=0;
             }
-            if(type==1){
+            if(type==0){
                 if(!e.empty()){
                     lines[cnt].push_back(make_pair(0,e));
                     e.clear();
@@ -82,7 +82,7 @@ void readlines(char* filename,vector<vector<pair<bool,string>>>&lines){
                 type=-type;
                 s+='\\';
             }
-            if(type==1){
+            if(type==0){
                 if(!e.empty()){
                     lines[cnt].push_back(make_pair(0,e));
                     e.clear();
@@ -99,7 +99,7 @@ void readlines(char* filename,vector<vector<pair<bool,string>>>&lines){
                 s+=c;
                 lines[cnt].push_back(make_pair(1,s));
                 s.clear();
-                type=1;
+                type=0;
             }
             else if(type==5||type==6||type==7)s+=c;
             else{
@@ -114,7 +114,7 @@ void readlines(char* filename,vector<vector<pair<bool,string>>>&lines){
                 s+='\\';
                 s+=c;
             }
-            else if(type==1){
+            else if(type==0){
                 if(!e.empty()){
                     lines[cnt].push_back(make_pair(0,e));
                     e.clear();
@@ -142,11 +142,11 @@ void readlines(char* filename,vector<vector<pair<bool,string>>>&lines){
                     s+=c;
                     lines[cnt].push_back(make_pair(0,s));
                     s.clear();
-                    type=1;
+                    type=0;
                 }
                 else s+=c;
             }
-            else if(type==4||type==5||type==7)s+=c;
+            else if(type==4||type==5||type==7||(type==1&&c=='.'))s+=c;
             else{
                 lines[cnt].push_back(make_pair(1,s));
                 type=3;
@@ -170,25 +170,31 @@ void readlines(char* filename,vector<vector<pair<bool,string>>>&lines){
                 type=-type;
                 s+='\\';
             }
-            if(type==1){
+            if(type==0){
                 if(!e.empty()){
                     lines[cnt].push_back(make_pair(0,e));
                     e.clear();
                 }
-                type=2;
-                s+=c;
+                type=(c<='9'&&c>='0')?1:2;
+                s=string(1,c);
             }
             else if(type==3){
-                lines[cnt].push_back(make_pair(1,s));
-                type=2;
-                s=string(1,c);
+                if(s=="-"&&c<='9'&&c>='0'){
+                    type=1;
+                    s+=c;
+                }
+                else{
+                    lines[cnt].push_back(make_pair(1,s));
+                    type=(c<='9'&&c>='0')?1:2;
+                    s=string(1,c);
+                }
             }
             else s+=c;
         }
         else if(type==4||type==5||type==6||type==7)s+=c;
     }
-    if(type!=1){
-        lines[cnt].push_back(make_pair(type==2||type==3,s));
+    if(type){
+        lines[cnt].push_back(make_pair(type==1||type==2||type==3,s));
     }
 }
 void work(char* filename){
@@ -214,7 +220,7 @@ void work(char* filename){
         fprintf(fp," %s\n",rk[i].second.c_str());
         icnt++;
     }
-    bool e=0;
+    bool e=1;
     for(auto&line:lines){
         for(auto&i:line)
             if(i.first){
